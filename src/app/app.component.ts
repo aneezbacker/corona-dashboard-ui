fb3a4e346e9acae6ce97dfba257efd7d8f055531
Aneez Backer <aneezbacker@gmail.com>
2020-03-18 04:02:31 +0800

    - edited analytics code - updated numbers

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,8 @@
 import {Component} from '@angular/core';
 import {Router, NavigationEnd} from '@angular/router';
+import {filter} from 'rxjs/operators';
 
-declare let gtag: Function;
+declare var gtag;
 
 @Component({
   selector: 'app-root',
@@ -13,12 +14,11 @@ export class AppComponent {
   constructor(public router: Router) {
 
     // for google-analytics
-    this.router.events.subscribe(event => {
-        if (event instanceof NavigationEnd) {
-          gtag('config', 'UA-160624854-1', {'page_path': event.urlAfterRedirects});
-        }
-      }
+    const navEndEvent$ = router.events.pipe(
+      filter(e => e instanceof NavigationEnd)
     );
-
+    navEndEvent$.subscribe((e: NavigationEnd) => {
+      gtag('config', 'UA-160624854-1', {'page_path': e.urlAfterRedirects});
+    });
   }
 }
