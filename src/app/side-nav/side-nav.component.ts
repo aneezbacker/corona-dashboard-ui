6c07e0e1607916d68046378eb4ddfe4b13ea8105
Aneez Backer <aneezbacker@gmail.com>
2020-03-23 02:08:56 +0800

    - adding share button - cosmetic changes

--- src/app/side-nav/side-nav.component.ts
+++ src/app/side-nav/side-nav.component.ts
@@ -1,5 +1,5 @@
 import {MediaMatcher} from '@angular/cdk/layout';
-import {ChangeDetectorRef, Component, OnDestroy} from '@angular/core';
+import {ChangeDetectorRef, Component, OnInit, OnDestroy} from '@angular/core';
 
 /** @title Responsive sidenav */
 @Component({
@@ -7,9 +7,13 @@ import {ChangeDetectorRef, Component, OnDestroy} from '@angular/core';
   templateUrl: './side-nav.component.html',
   styleUrls: ['./side-nav.component.css']
 })
-export class SideNavComponent implements OnDestroy {
+export class SideNavComponent implements OnInit, OnDestroy {
 
   mediaQueryList: MediaQueryList;
+  webshareApiPresent: boolean;
+
+
+  shareNavigator: any = window.navigator;
 
   private mediaQueryListener: () => void;
 
@@ -19,18 +23,26 @@ export class SideNavComponent implements OnDestroy {
     this.mediaQueryList.addListener(this.mediaQueryListener);
   }
 
-  /*
+
   ngOnInit(): void {
-    console.log('SideNavComponent::ngOnInit. shouldRun:' + shouldRun);
+    this.webshareApiPresent = window.navigator && window.navigator['share'];
   }
-  */
+
 
   ngOnDestroy(): void {
     this.mediaQueryList.removeListener(this.mediaQueryListener);
- //   console.log('SideNavComponent::ngOnDestroy. shouldRun:' + shouldRun);
+    //   console.log('SideNavComponent::ngOnDestroy. shouldRun:' + shouldRun);
   }
 
-
-
-
+  share(): void {
+    if (this.webshareApiPresent) {
+      window.navigator['share']({
+        title: document.title,
+        url: window.location.href
+      }).then(() => {
+      })
+        .catch(console.error);
+    } else {
+    }
+  }
 }
